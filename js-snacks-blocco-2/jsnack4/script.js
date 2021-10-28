e9dac81e6bf7706514e8cf4e23f5939a8a5855c0
Bonu93 <luca.morucci.kb@gmail.com>
2021-10-28 18:12:11 +0200

    complete

---
+++ js-snacks-blocco-2/jsnack4/script.js
@@ -0,0 +1,24 @@
+// JSNACK 4
+
+// Crea due array che hanno un numero di elementi diversi.
+// Aggiungi elementi casuali all’array che ha meno elementi,
+// fino a quando ne avrà tanti quanti l’altro
+
+array1 = [3, 5, 7, 9, 11, 43, 55, 12];
+array2 = [5, 3, 1, 11];
+
+
+
+while (array1.length != array2.length) {
+    if (array1.length > array2.length) {
+        array2.push(Math.floor(Math.random() * 100))
+    } else {
+        array1.push(Math.floor(Math.random() * 100))
+    }
+}
+
+console.log(array1);
+console.log(array2);
+
+console.log(array1.length);
+console.log(array2.length);
---
+++ pomeriggio/script.js
@@ -0,0 +1,35 @@
+// Ogni volta che si clicca un bottone, il nostro programma chiederà un numero di 4 cifre all’utente.
+// Se l’utente ha inserito un numero corretto, allora il programma calcolerà la somma e le media di tutte le cifre che lo compongono stampandola nel DOM, altrimenti ripeterà la richiesta di inserimento finché non avrà inserito un numero corretto
+
+// dom refs 
+const btn = document.querySelector('.btn');
+
+const printMedia = document.querySelector('.media');
+
+const printSomma = document.querySelector('.somma');
+
+// click 
+btn.addEventListener('click', function() {
+
+    let media = 0;
+    let somma = 0;
+    
+    let num = prompt('Inserisci un numero di 4 cifre');
+
+    while (num.length !== 4 || isNaN(num)) {
+        alert('Inserisci un numero di 4 cifre!!!')
+        num = prompt('Inserisci un numero di 4 cifre');
+    }
+
+    for (i = 0; i < num.length; i++) {
+        somma += parseInt(num[i])
+    }
+    
+    media = somma/num.length
+
+    // print in dom 
+    printMedia.innerHTML = `La media delle cifre che hai inserito è: ${media}`
+
+    printSomma.innerHTML = `La somma delle cifre che hai inserito è: ${somma}`
+
+})
